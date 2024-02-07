652135ed0c5a1de4addf8d4b9ed260d1d6233762
Aaron-Paranoid-Android <legoaaron.ford@gmail.com>
2024-02-07 10:43:02 -0600

    variables and constructors

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    private int HighestAttempts;
+    private int id;
+    private static int lastNumber;
+    private int lowestAttempts;
+    private java.lang.string name;
+    private int wins;
+public Player(){
+
+}
+
+public Player(java.lang.String name){
+
+}
+
+
+
+}
